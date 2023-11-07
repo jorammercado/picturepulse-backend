8e3184ec511cea4569edfa6f81147e1b7a63c8ff
arim83 <jorammercado@pursuit.org>
2023-11-07 10:15:56 -0500

    schema script complete, working on seed

---
+++ db/schema.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS movies_dev;
+CREATE DATABASE movies_dev;
+
+\c movies_dev;
+
+CREATE TABLE movies (
+ id SERIAL PRIMARY KEY,
+ movie_name VARCHAR(20) NOT NULL,
+ poster_link TEXT,
+ studio VARCHAR(20),
+ director VARCHAR(20),
+ staring VARCHAR(20),
+ overview TEXT,
+ runtime INT DEFAULT 0,
+ release_year INT,
+ budget INT,
+ current_balance INT DEFAULT 0,
+ schedule TEXT,
+ genre TEXT,
+ in_production BOOLEAN DEFAULT false
+);
+
+CREATE TABLE actors (
+    id SERIAL PRIMARY KEY,
+    actor_name TEXT NOT NULL,
+    actor_img TEXT,
+    active DEFAULT false,
+    age INT,
+    movie_id INTEGER REFERENCES movies (id)
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    task_name VARCHAR(20) NOT NULL,
+    description TEXT NOT NULL,
+    department TEXT,
+    cost INT DEFAULT 0,
+    completed BOOLEAN DEFAULT false,
+     movie_id INTEGER REFERENCES movies (id)
+     ON DELETE CASCADE
+);
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+\c movies_dev;
+
+INSERT INTO movies(movie_name, poster_link, studio, director, staring, overview,
+                   runtime, release_year, budget, current_balance, schedule,
+                   genre, in_production) VALUES
+(),
+(),
+();
+
+INSERT INTO actors()

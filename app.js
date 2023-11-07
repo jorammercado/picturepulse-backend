fce7853478b4a340ee836adf9e2432e75fb4e9f5
arim83 <jorammercado@pursuit.org>
2023-11-07 13:38:37 -0500

    remove reference comments

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ app.get("/", (req, res) => {
 
 const moviesController = require("./controllers/moviesController.js")
 app.use("/movies", moviesController)
-// const actorsController = require("./controllers/actorsController.js")
-// app.use("/actors", tasksController)
-// const tasksController = require("./controllers/tasksController.js")
-// app.use("/actors", actorsController)
 
 app.get("*", (req, res) => {
     res.status(404).send("Page not found")
